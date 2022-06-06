cd56e9eb5060153487285df388c73332e26b9ad4
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2022-06-06 16:51:20 -0400

    Create logoff.bat

---
+++ logoff.bat
@@ -0,0 +1,3 @@
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\S-1-5-21-1054529746-1597168808-3339453314-1000" /f /va
+RD C:\Users\RWCI /S /Q
+RD C:\Users\RWCI.%computername% /S /Q
