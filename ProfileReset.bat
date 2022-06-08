3b196974753cb583e344dd6fdf1e80dccd170f66
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2022-06-08 10:17:09 -0400

    Update ProfileReset.bat

--- ProfileReset.bat
+++ ProfileReset.bat
@@ -2,3 +2,4 @@ for /f "Tokens=*" %%I in ('powershell -command " & {get-localuser RWCI | select-
 REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\%UserSID%" /f /va
 RD C:\Users\RWCI /S /Q
 RD C:\Users\RWCI.%computername% /S /Q
+RD C:\$Recycle.bin /S /Q
