344abb712c20d5f3af2d90e190e6703faf1a084e
jgaines84 <93670137+jgaines84@users.noreply.github.com>
2022-06-10 16:18:36 -0400

    Update RWCI_LAB.bat

--- RWCI_LAB.bat
+++ RWCI_LAB.bat
@@ -1,4 +1,4 @@
-SET Version=Version 3.98
+SET Version=Version 3.99
 IF NOT EXIST C:\Apps MD C:\Apps
 ATTRIB C:\Apps +S +H
 ECHO. >> C:\Apps\log.txt
@@ -8,6 +8,7 @@ ECHO %time% - Start >> C:\Apps\log.txt
 
 CALL :UpdateTimeZone
 CALL :SleepSettings
+CALL :Windows11Block
 CALL :CheckInternet
 CALL :UpdateMain
 CALL :Recovery
@@ -31,6 +32,14 @@ ECHO %time% - Test Started >> C:\Apps\log.txt
 ECHO %time% - Test Finished >> C:\Apps\log.txt
 ECHO %time% - Finish >> C:\Apps\log.txt
 EXIT
+
+::Windows11Block-------------------------------------------------------
+:Windows11Block
+ECHO %time% - Windows11Block - Start >> C:\Apps\log.txt
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v TargetReleaseVersion /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v TargetReleaseVersionInfo /t REG_SZ /d 21H2 /f
+ECHO %time% - Windows11Block - Finish >> C:\Apps\log.txt
+EXIT /b
 
 ::UpdateTimeZone--------------------------------------------------------------------
 :UpdateTimeZone
